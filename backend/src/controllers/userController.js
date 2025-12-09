e79d5f729d5c68e85a4d855d8416ff0b154672c2
Vipin <vipinbhardwal286@gmail.com>
2025-12-09 19:19:26 +0530

    strong logics updated for login and register

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,10 +5,24 @@ import jwt from 'jsonwebtoken'
 export const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
+        // 1. Validate input
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
 
+        // 2. Check if email exists
         const userExist = await User.findOne({ email });
 
         if (userExist) return res.status(400).json({ message: "Email already registered." })
+
+
+        // check password strength
+        const strongPassword_Regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (!strongPassword_Regex.test(password)) {
+            return res.status(400).json({
+                message: "Password must be at least 8 characters long and include:1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character."
+            });
+        }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -17,7 +31,15 @@ export const registerUser = async (req, res) => {
             email,
             password: hashedPassword
         });
-        res.send(201).json({ message: "user registered", user });
+        const safeUser = {
+            id: user._id,
+            name: user.name,
+            email: user.email
+        };
+        return res.status(201).json({
+            message: "User registered",
+            user: safeUser
+        });
 
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -47,6 +69,7 @@ export const loginUser = async (req, res) => {
             process.env.JWT_SECRET_KEY,
             { expiresIn: "7d" }
         );
+
         res.json({
             message: "Login successful",
             token,
